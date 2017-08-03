c1b9247a5cabc731642580b5d4f6591a38ea1fdb
Anondo <aanondos@gmail.com>
2017-08-04 03:32:35 +0600

    added the get_queryset method in the ListView

--- builtinViews/genericView/views.py
+++ builtinViews/genericView/views.py
@@ -9,6 +9,9 @@ class BooklistView(generic.ListView): #the ListView is used for displaying objec
     model = Books #sets the model and determiens which objects to display
     context_object_name = "books" #sets the context object name,by default it is set "object_list"
 
+    def get_queryset(self): #determines which objects to send and how
+        return Books.objects.all().order_by("-price")
+
 class BookDetailView(generic.DetailView):
     model = Books
     context_object_name = "book" #by default it is set, whatever the name of the model is in its lowercase i.e for this case it would have been books
